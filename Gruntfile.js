04ac83a4f37c15c606f6dd49a998b7e0991f65f7
Matthew Clark <mclark@indeed.com>
2017-10-03 23:53:17 -0500

    Initial commit.  Have grunt working.  Even have grunt-embed working.  Now need to import content from other branch.

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    "dest/css/skeleton.css": "src/scss/skeleton.scss"
+                }
+            }
+        },
+        embed: {
+          home: {
+            options: {
+              threshold: '15KB'
+            },
+            files: {
+              'dest/index.html': 'src/index.html'
+            }
+          }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            styles: {
+                files: ['src/scss/**/*.scss'], // which files to watch
+                tasks: ['sass','embed'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-embed');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['sass','embed','watch']);
+};
